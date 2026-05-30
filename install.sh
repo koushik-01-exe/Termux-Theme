f3374a941b28d58dad9d06a879419866c72e08ba
koushik-01-exe <sadhukhankoushik963@gmail.com>
2026-05-30 11:33:25 +0530

    fix install script

--- install.sh
+++ install.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
+set -e
+
+echo "Installing packages..."
 pkg update -y
 pkg install git fish starship neofetch -y
 
+echo "Setting up config files..."
 mkdir -p ~/.config/neofetch ~/.config/fish ~/.termux
 
-cp neofetch-config.conf ~/.config/neofetch/config.conf
-cp sword.txt ~/.config/neofetch/
-cp starship.toml ~/.config/
-cp config.fish ~/.config/fish/
-cp termux.properties ~/.termux/
-cp colors.properties ~/.termux/
-cp .bashrc ~/
+REPO="https://raw.githubusercontent.com/koushik-01-exe/Termux-Theme/main"
+
+curl -sL "$REPO/neofetch-config.conf" -o ~/.config/neofetch/config.conf
+curl -sL "$REPO/sword.txt" -o ~/.config/neofetch/sword.txt
+curl -sL "$REPO/starship.toml" -o ~/.config/starship.toml
+curl -sL "$REPO/config.fish" -o ~/.config/fish/config.fish
+curl -sL "$REPO/termux.properties" -o ~/.termux/termux.properties
+curl -sL "$REPO/colors.properties" -o ~/.termux/colors.properties
+curl -sL "$REPO/.bashrc" -o ~/.bashrc
 
 chsh -s fish
 termux-reload-settings
-echo "Done!"
+
+echo "Done! Restart Termux ⚡"
